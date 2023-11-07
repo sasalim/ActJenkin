e3437beaa90f368108799c6c3d24a3705fd4bfe7
Salim Saafi <ssaafi@dom.tti>
2023-11-07 17:09:36 +0100

    corr

--- src/test/java/steps/runner.java
+++ src/test/java/steps/runner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		features="src/test/resources/features",
 		glue= {"steps"},monochrome=true, strict=true,
 		tags="@tag1",
-		plugin = {"pretty", "json:target/reports/cucumber-report.json",
+		plugin = {"pretty", "json:target/rapport.json",
                 "html:target/reports/cucumber.html"}
 		)
 
